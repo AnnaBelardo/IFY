cb758fc0d9f73fabd76f64db7aced67955448b12
gcastaldo10 <g.castaldo10@studenti.unisa.it>
2019-12-18 14:42:43 +0100

    aggiunta classi StudenteForm e StudenteFormValidator

---
+++ src/main/java/it/unisa/di/is/gc1/ify/web/StudenteForm.java
@@ -0,0 +1,101 @@
+package it.unisa.di.is.gc1.ify.web;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+
+/**
+ * Oggetto utilizzato per mappare i campi di un form di uno studente. Questo oggetto
+ * viene passato come parametro al controller dalla dispatcher servlet quando un utente 
+ * sottomette il modulo di registrazione.
+ * 
+ * @author Giusy Castaldo, Alessia Natale
+ */
+public abstract class StudenteForm {
+	
+	public StudenteForm() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public LocalDate getDataNascita() {
+		return dataNascita;
+	}
+	public void setDataNascita(LocalDate dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+	public String getSesso() {
+		return sesso;
+	}
+	public void setSesso(String sesso) {
+		this.sesso = sesso;
+	}
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfermaPsw() {
+		return confermaPsw;
+	}
+	public void setConfermaPsw(String confermaPsw) {
+		this.confermaPsw = confermaPsw;
+	}
+	public String isCondizioni() {
+		return condizioni;
+	}
+	public void setCondizioni(String condizioni) {
+		this.condizioni = condizioni;
+	}
+
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	private String telefono;
+	private LocalDate dataNascita;
+	private String sesso;
+	private String matricola;
+	private String email;
+	private String password;
+	private String confermaPsw;
+	private String condizioni;
+}
+
+
+
---
+++ src/main/java/it/unisa/di/is/gc1/ify/web/StudenteFormValidator.java
@@ -0,0 +1,52 @@
+package it.unisa.di.is.gc1.ify.web;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.executable.ExecutableValidator;
+import javax.validation.metadata.BeanDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import it.unisa.di.is.gc1.ify.Studente.RichiestaIscrizioneNonValidaException;
+import it.unisa.di.is.gc1.ify.Studente.RichiestaIscrizioneService;
+
+/**
+ * Classe che definisce un validatore per {@link StudenteForm} tramite i servizi offerti da 
+ * {@link RichiestaIscrizioneService}. Il controllo effettuato riguarda la validità dei campi
+ * definiti nell'entità Studente. 
+ * 
+ * @see StudenteForm 
+ * @see RichiestaIscrizioneService
+ * 
+ * @author Giusy Castaldo, Alessia Natale
+ *
+ */
+@Component
+public class StudenteFormValidator implements Validator {
+
+	@Autowired
+	private RichiestaIscrizioneService richiestaIscrizioneService;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return StudenteForm.class.isAssignableFrom(clazz);
+	}
+	
+	/**
+	 * Effettua la validazione dell'oggetto target riportando gli errori nell'oggetto errors.
+	 * 
+	 * @param target Oggetto da validare
+	 * @param errors Oggetto in cui salvare l'esito della validazione
+	 */
+	@Override
+	public void validate(Object target, Errors errors) {
+		StudenteForm form = (StudenteForm) target;
+		try {
+			richiestaIscrizioneService.validaRichiestaIscrizione(form);
+		} catch (RichiestaIscrizioneNonValidaException) {
+			errors.reject("form", "Campi form non validi");
+		}
+	}
+
+}
